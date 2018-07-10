1c01ebd7d504b340751f6d4b4c62fda426999fc2
GTaveras <gtaveras@browning.edu>
2018-07-10 11:59:31 -0400

    Random Int Challenge Completed

--- random-int-challenge.js
+++ random-int-challenge.js
@@ -1,9 +1,3 @@
 //Author: Giovanni Taveras
-var randomInt = require('random-int');
-console.log(random)
 
-randomInt(5);
-//=> 3
-
-randomInt(10, 100);
-//=> 54
+console.log(Math.round(Math.random() * 6))
